306a11437e950db712e2401a2ed7c28c477e5ba7
ta234vi <tanvi2005garg@gmail.com>
2026-04-22 15:05:17 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,17 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///attendance.db"
+# This ensures the database is created in the same folder as your code
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, "attendance.db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(bind=engine)
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{db_path}"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
